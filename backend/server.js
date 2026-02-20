aaceb453cad5e193bef84cad64fa3b76c4fafc0d
Sahana S <sahana.tvsathya@gmail.com>
2026-02-20 17:54:25 +0530

    fixed backend connection issue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: '*'
+  origin: [
+    "http://localhost:5173",
+    "https://banking-website-eight.vercel.app"
+  ],
+  credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
